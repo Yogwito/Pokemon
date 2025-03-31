df9420af450efd374a308b30fb96e7c770ddcb37
Juan José Trujillo <juanj.trujillo@autonoma.edu.co>
2025-03-31 18:00:44 -0500

    Se crearon y se documentaron todas las interfaces

---
+++ src/interfaces/Pokemon/PokemonElectrico.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces.Pokemon;
+
+/**
+ *
+ * @author User
+ */
+/**
+ * Interfaz que define los ataques de un Pokémon tipo Fuego.
+ */
+public interface PokemonElectrico {
+    void atacarImpactrueno();
+    void atacarPunioTrueno();
+    void atacarRayo();
+    void atacarRayoCarga();
+}
---
+++ src/interfaces/Pokemon/PokemonFuego.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces.Pokemon;
+
+/**
+ *
+ * @author User
+ */
+public interface PokemonFuego {
+    void atacarPunioFuego();
+    void atacarAscuas();
+    void atacarLanzallamas();
+}
